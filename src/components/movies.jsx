c8776b812f7edc6a7f52a6e4c08c56a2ed885518
Ebenezer <ebene2009@gmail.com>
2020-08-05 06:55:52 +0100

    movie lister

---
+++ src/components/movies.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    console.log(movie);
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: movieCount } = this.state.movies;
+    if (movieCount === 0) return <h4> There are no movies in the database</h4>;
+    return (
+      <React.Fragment>
+        <h4>Showing {movieCount} movies in the database</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <th scope="row">{movie.title}</th>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+
+  //   movieMessage() {
+  //     const msg = 0;
+  //     return  movieCount === 0 ? (
+  //       <h4>There are no movies to show</h4>
+  //     ) : (
+  //       msg
+  //     );
+  //   }
+}
+
+export default Movies;

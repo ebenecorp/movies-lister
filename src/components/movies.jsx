6658b196394560bffedfb41c76b9111e2ca883a3
Ebenezer <ebene2009@gmail.com>
2020-08-25 05:58:31 +0100

    Frontend Pagination implemented

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,28 +1,38 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like"
-
+import Like from "./common/like";
+import Paginatiion from "./common/pagination";
+import { paginate } from "../utils/paginate";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
-    console.log(movie);
+    // console.log(movie);
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
   };
 
-  handleLike = (movie)=>{
+  handleLike = (movie) => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
-    movies[index]= {...movies[index]};
-    movies[index].liked = !movies[index].liked
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
     return this.setState({ movies });
-  }
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
 
   render() {
     const { length: movieCount } = this.state.movies;
+    const { pageSize, currentPage, movies:allMovies } = this.state;
+    const movies = paginate(allMovies, pageSize, currentPage);
+
     if (movieCount === 0) return <h4> There are no movies in the database</h4>;
     return (
       <React.Fragment>
@@ -39,13 +49,18 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <th scope="row">{movie.title}</th>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
-                <td><Like onClick={()=> this.handleLike(movie)} liked={movie.liked} /></td>
+                <td>
+                  <Like
+                    onClick={() => this.handleLike(movie)}
+                    liked={movie.liked}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
@@ -58,6 +73,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Paginatiion
+          itemsCount={movieCount}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          currentPage={currentPage}
+        />
       </React.Fragment>
     );
   }
---
+++ src/utils/paginate.js
@@ -0,0 +1,7 @@
+import _ from "lodash";
+
+export function paginate(items, pageSize, pageNumber) {
+  const startIndex = (pageNumber - 1) * pageSize;
+
+  return _(items).slice(startIndex).take(pageSize).value();
+}
